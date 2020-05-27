33df21465e9e101331a481403e1d044eacc45997
Daniel Fróes <danielfroes15@gmail.com>
2020-05-27 08:58:30 -0300

    draw animation

---
+++ Assets/Scripts/DrawCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCard : MonoBehaviour
+{
+
+    [SerializeField] private float drawSpeed;
+    [SerializeField] private Transform deckTransform;
+    private Vector3 _targetPosition;
+
+    private void Awake() 
+    {
+        _targetPosition = transform.position;
+    }
+
+    private void OnEnable()
+    {
+        transform.position = deckTransform.position;
+    }
+ 
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, drawSpeed*Time.deltaTime);
+    }
+
+    
+
+}
